940dc797e37f1c69fe0bbb43f457f8a15b53124a
prinxeps <tig00125@gmail.com>
2024-08-01 10:58:06 +0900

    게임 정상종료, 강제종료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -45,6 +45,52 @@ fun isValidInput(input: String): Boolean {
 }
 
 
+fun startGame() {
+    var continuePlaying = true
+    while (continuePlaying) {
+        val secretNumber = generateRandomNumber()
+
+        while (true) {
+            val input = getUserInput("숫자 3개를 입력하세요 (1-9, 서로 다른 숫자):")
+
+            if (!isValidInput(input)) {
+                throw IllegalArgumentException("잘못된 입력입니다. 1에서 9까지의 서로 다른 3자리 숫자를 입력해 주세요.")
+            }
+
+            val guess = input.map { it.toString().toInt() }
+            val (strikes, balls) = calculateStrikesAndBalls(secretNumber, guess)
+
+            when {
+                strikes == 3 -> {
+                    println("3스트라이크! 3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+                    break
+                }
+                strikes > 0 || balls > 0 -> {
+                    println("${balls}볼 ${strikes}스트라이크")
+                }
+                else -> println("낫싱")
+            }
+        }
+
+        continuePlaying = handleRestart()
+    }
+}
+
+fun handleRestart(): Boolean {
+    val restartInput = getUserInput("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    return when (restartInput) {
+        "1" -> true
+        "2" -> {
+            println("게임 종료")
+            false
+        }
+        else -> {
+            throw IllegalArgumentException("잘못된 입력입니다. 1 또는 2를 입력해 주세요.")
+        }
+    }
+}
+
+
 fun main() {
     println("숫자 야구 게임을 시작합니다!")
     startGame()
